1dc6ccf8c0802c6b857a65ffc4b80e52b60df050
Joyce Lima Avelino <joyce.avelino@aluno.uepb.edu.br>
2024-07-16 22:42:05 -0300

    Starting change theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         {isThemesVisible && <Themes toggleThemes={toggleThemes} />}
       </div>
     </div>
+    
   );
 }
 
--- frontend/src/components/Themes/Themes.jsx
+++ frontend/src/components/Themes/Themes.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './Themes.css'
 import { faCaretRight, faCaretLeft, faMinus } from '@fortawesome/free-solid-svg-icons'
 import Draggable from 'react-draggable'
+import './Themes.css'
+
 
 export default function Themes({ toggleThemes }) {
+
   
+  const [imageSets, setImageSets] = useState({
+    darkAcademia: [],
+    lofi: []
+  });
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentSet, setCurrentSet] = useState('images');
+
+  useEffect(() => {
+    // Fetch the JSON data dynamically
+    fetch('../../../public/images.json') // Replace with the correct path to your JSON file
+      .then(response => response.json())
+      .then(data => {
+        setImageSets(data);
+
+      })
+      .catch(error => console.error('Error fetching images:', error));
+
+
+  }, []);
+
+  const handleNextImage = (imageSet) => {
+    setCurrentImageIndex(prevIndex => (prevIndex + 1) % imageSets[imageSet].length);
+    document.body.style.backgroundImage = `url(${imageSets[imageSet][currentImageIndex]})`;
+  };
+
+  const handlePrevImage = (imageSet) => {
+    setCurrentImageIndex(prevIndex => (prevIndex - 1 + imageSets[imageSet].length) % imageSets[imageSet].length);
+    document.body.style.backgroundImage = `url(${imageSets[imageSet][currentImageIndex]})`;
+  };
+
+
   return (
     <Draggable handle=".modal-title">
     <div className="themes-modal">
@@ -16,15 +50,15 @@ export default function Themes({ toggleThemes }) {
             <p>Themes</p>
       </div>
       <ul className="themes-menu">
-        <li className="menu-item">
-          <div className="arrow-item-left"><FontAwesomeIcon icon={faCaretLeft} /></div>
+        <li className="menu-item"  >
+          <div className="arrow-item-left" onClick={() => handlePrevImage('darkAcademia')} ><FontAwesomeIcon icon={faCaretLeft} /></div>
           <p>Dark academia</p>
-          <div className="arrow-item-right"><FontAwesomeIcon icon={faCaretRight} /> </div>
+          <div className="arrow-item-right" onClick={() => handleNextImage('darkAcademia')} ><FontAwesomeIcon icon={faCaretRight} /> </div>
         </li>
-        <li className="menu-item">
-        <div className="arrow-item-left"><FontAwesomeIcon icon={faCaretLeft} /></div>
+        <li className="menu-item" >
+        <div className="arrow-item-left" onClick={() => handlePrevImage('lofi')} ><FontAwesomeIcon icon={faCaretLeft} /></div>
           <p> lo-fi aesthetic</p>
-          <div className="arrow-item-right"><FontAwesomeIcon icon={faCaretRight} /> </div>
+          <div className="arrow-item-right" onClick={() => handleNextImage('lofi')} ><FontAwesomeIcon icon={faCaretRight} /> </div>
         </li>
         <li className="menu-item">
         <div className="arrow-item-left"><FontAwesomeIcon icon={faCaretLeft} /></div>
